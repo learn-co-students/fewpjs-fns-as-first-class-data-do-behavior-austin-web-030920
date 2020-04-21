cc1e8023073d2a32f013eac311a3d1f63c09700c
Josh-Gotro <joshuagauthreaux@gmail.com>
2020-04-20 21:45:15 -0500

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,29 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+
 /* Write your implementation of greet() */
+function greet(string){
+// split time and capture first two numbers
+var hour = string.split(":")
+// if before 12 good morning
+  if (hour[0] < 12) {
+  message = "Good Morning"
+  // else if 17 or later good evening
+  } else if (hour[0]  >= 17) {
+  message = "Good Evening"
+  // else good afternoon
+  } else { 
+  message = "Good Afternoon"
+  }
+// return display message
+return displayMessage(message)
+}
+
 /* Write your implementation of displayMessage() */
+function displayMessage() {
+  const input = document.getElementById('time');
+  time.addEventListener('click',function(event){
+    alert(message)
+  });
+}
